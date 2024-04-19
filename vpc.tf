4cb64a76c2131ad94b17e03ef7a498bef0205248
elchibek123 <elchibek.kamalov@gmail.com>
2024-04-18 23:10:06 -0400

    added db subnet

--- vpc.tf
+++ vpc.tf
@@ -4,6 +4,7 @@ module "vpc" {
 
   cidr_block                 = var.cidr_block
   public_subnets             = var.public_subnets
+  private_subnets            = var.private_subnets
   azs                        = data.aws_availability_zones.available.names
   map_public_ip_on_launch    = true
   create_internet_gateway    = true
@@ -15,4 +16,16 @@ module "vpc" {
   enable_dns_support         = true
   env                        = var.environment
   region_name                = var.aws_region
+}
+
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name        = "db-${var.environment}-${var.aws_region}-vpc-subnet"
+  description = "Subnet group for the application's RDS instance"
+  subnet_ids  = module.vpc.private_subnets[*]
+
+  tags = merge(
+    {
+      Name = "db-${var.environment}-${var.aws_region}-vpc-subnet"
+    }
+  )
 }
