579ecc09d66cdb33f0222f07f22973c391132e22
elchibek123 <elchibek.kamalov@gmail.com>
2024-04-28 16:41:36 -0400

    test

--- cluster-infrastructure/providers.tf
+++ cluster-infrastructure/providers.tf
@@ -20,3 +20,9 @@ provider "aws" {
     }
   }
 }
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data))
+  token                  = data.aws_eks_cluster_auth.cluster_auth.token
+}
