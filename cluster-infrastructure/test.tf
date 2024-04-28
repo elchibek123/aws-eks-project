b74eb824792e1faf94bdd73cbda107883beab033
elchibek123 <elchibek.kamalov@gmail.com>
2024-04-28 17:25:15 -0400

    updated resources

---
+++ cluster-infrastructure/test.tf
@@ -0,0 +1,29 @@
+resource "kubernetes_cluster_role" "reader" {
+  metadata {
+    name = "reader"
+  }
+
+  rule {
+    api_groups = ["*"]
+    resources  = ["deployments", "configmaps", "pods", "secrets", "services"]
+    verbs      = ["get", "list", "watch"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "reader" {
+  metadata {
+    name = "reader"
+  }
+
+  subject {
+    kind      = "Group"
+    name      = "reader"
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  role_ref {
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.reader.metadata.0.name
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
