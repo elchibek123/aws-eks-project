fbd7127302a39637d49a4ee538502a47c3090597
elchibel123 <elchibek.kamalov@gmail.com>
2024-04-29 14:37:48 -0400

    added irsa

---
+++ kubernetes-manifest/iam-role-and-policy-irsa.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_role" "irsa_iam_role" {
+  name = "${var.environment}-${var.aws_region}-iam-role-irsa"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Federated = "${data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_arn}"
+        }
+        Condition = {
+          StringEquals = {            
+            "${data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_extract_from_arn}:sub": "system:serviceaccount:default:irsa-demo-sa"
+          }
+        }        
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "${var.environment}-${var.aws_region}-iam-role-irsa"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "irsa_iam_role_policy_attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  role       = aws_iam_role.irsa_iam_role.name
+}
+
+output "irsa_iam_role_arn" {
+  description = "IRSA Demo IAM Role ARN"
+  value = aws_iam_role.irsa_iam_role.arn
+}
